3e34474b5438d90206a0ac05ede32229de964398
Giovanni Petrantoni <giovanni.petrantoni@siliconstudio.co.jp>
2015-11-13 17:02:07 +0900

    [Tests] converted test to new simplified api

--- Tests/JumpyJetTest/JumpyJetTest.cs
+++ Tests/JumpyJetTest/JumpyJetTest.cs
@@ -1,14 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using NUnit.Framework;
-using SiliconStudio;
+﻿using NUnit.Framework;
 using SiliconStudio.Core;
 using SiliconStudio.Core.Mathematics;
-using SiliconStudio.Xenko.Engine.Network;
 using SiliconStudio.Xenko.Input;
 using SiliconStudio.Xenko.Testing;
 
@@ -17,94 +9,30 @@ namespace JumpyJetTest
     [TestFixture]
     public class JumpyJetTest
     {
-        public SocketMessageLayer StartGame()
-        {
-            var url = $"/service/{XenkoVersion.CurrentAsText}/SiliconStudio.Xenko.SamplesTestServer.exe";
-
-            var socketContext = RouterClient.RequestServer(url).Result;
-
-            var success = false;
-            var message = "";
-            var ev = new AutoResetEvent(false);
-
-            var socket = new SocketMessageLayer(socketContext, false);
-
-            socket.AddPacketHandler<StatusMessageRequest>(request =>
-            {
-                success = !request.Error;
-                message = request.Message;
-                ev.Set();
-            });
-
-            socket.AddPacketHandler<LogRequest>(request =>
-            {
-                Console.WriteLine(request.Message);
-            });
-
-            Task.Run(() => socket.MessageLoop());
-
-            var xenkoDir = Environment.GetEnvironmentVariable("SiliconStudioXenkoDir");
-
-            socket.Send(new TestRegistrationRequest
-            {
-                Platform = (int)PlatformType.Windows,
-                Tester = true,
-                Cmd = xenkoDir + "\\samples\\Games\\JumpyJet\\Bin\\Windows-Direct3D11\\Debug\\JumpyJet.exe"
-            }).Wait();
-
-            if (!ev.WaitOne(10000))
-            {
-                throw new Exception("Time out while launching the game");
-            }
-
-            if (!success)
-            {
-                throw new Exception("Failed: " + message);
-            }
-
-            Console.WriteLine("Game started. (message: " + message + ")");
-
-            return socket;
-        }
+        private const string Path = "samples\\Games\\JumpyJet\\Bin\\Windows-Direct3D11\\Debug\\JumpyJet.exe";
 
         [Test]
         public void TestLaunch()
         {
-            var socket = StartGame();
-
-            Task.Delay(2000).Wait();
-
-            socket.Send(new TestEndedRequest()).Wait();
+            using (var game = new GameTest(Path, PlatformType.Windows))
+            {
+                game.Wait(2000);
+            }
         }
 
         [Test]
         public void TestInputs()
         {
-            var socket = StartGame();           
-
-            Task.Delay(2000).Wait();
-
-            socket.Send(new TapSimulationRequest { Down = true, Coords = new Vector2(0.5f, 0.7f) }).Wait();
-            Console.WriteLine("TapSimulationRequest Down.");
-            Task.Delay(500).Wait();
-            socket.Send(new TapSimulationRequest { Down = false, Coords = new Vector2(0.5f, 0.7f) }).Wait();
-            Console.WriteLine("TapSimulationRequest Up.");
-            Task.Delay(500).Wait();
-
-            socket.Send(new KeySimulationRequest {Down = true, Key = Keys.Space}).Wait();
-            Console.WriteLine("KeySimulationRequest Down.");
-            Task.Delay(500).Wait();
-            socket.Send(new KeySimulationRequest { Down = false, Key = Keys.Space }).Wait();
-            Console.WriteLine("KeySimulationRequest Up.");
-            Task.Delay(500).Wait();
-
-            var xenkoDir = Environment.GetEnvironmentVariable("SiliconStudioXenkoDir");
-
-            socket.Send(new ScreenshotRequest {Filename = xenkoDir + "\\screenshots\\JumpyJet.png" }).Wait();
-            Console.WriteLine("ScreenshotRequest.");
-            Task.Delay(500).Wait();
-
-            socket.Send(new TestEndedRequest()).Wait();
+            using (var game = new GameTest(Path, PlatformType.Windows))
+            {
+                game.Wait(2000);
+                game.Tap(new Vector2(0.5f, 0.7f), 500);
+                game.Wait(500);
+                game.KeyPress(Keys.Space, 500);
+                game.Wait(500);
+                game.TakeScreenshot();
+                game.Wait(500);
+            }
         }
     }
 }
