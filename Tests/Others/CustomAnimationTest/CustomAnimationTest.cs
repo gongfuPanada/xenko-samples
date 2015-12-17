90806f835d2b4680b3d600ea2b321473fbbcdff4
Jörg Wollenschläger <joerg.wollenschlaeger@siliconstudio.co.jp>
2015-12-17 14:17:17 +0900

    [CustomAnimation] Add new sample

---
+++ Tests/Others/CustomAnimationTest/CustomAnimationTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+using SiliconStudio.Core;
+using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Xenko.Input;
+using SiliconStudio.Xenko.Games.Testing;
+
+namespace CustomAnimationTest
+{
+    [TestFixture]
+    public class CustomAnimationTest
+    {
+        private const string Path = "samples\\Others\\CustomAnimation\\Bin\\Windows\\Debug\\CustomAnimation.exe";
+
+#if TEST_ANDROID
+        private const PlatformType TestPlatform = PlatformType.Android;
+#else
+        private const PlatformType TestPlatform = PlatformType.Windows;
+#endif
+
+        [Test]
+        public void TestLaunch()
+        {
+            using (var game = new GameTestingClient(Path, TestPlatform))
+            {
+                game.Wait(TimeSpan.FromMilliseconds(2000));
+            }
+        }
+
+        [Test]
+        public void TestInputs()
+        {
+            using (var game = new GameTestingClient(Path, TestPlatform))
+            {
+                game.Wait(TimeSpan.FromMilliseconds(2000));
+
+                game.TakeScreenshot();
+
+                game.Wait(TimeSpan.FromMilliseconds(20000));
+            }
+        }
+    }
+}
