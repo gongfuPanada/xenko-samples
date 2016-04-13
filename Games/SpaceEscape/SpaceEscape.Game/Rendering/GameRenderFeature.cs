d0170fa511e7a4ccfc4853e76a4d2f87f795d881
Virgile Bello <virgile@siliconstudio.co.jp>
2016-04-13 18:27:52 +0900

    [SpaceEscape] Fix sample with RenderThreadContext removal

--- Games/SpaceEscape/SpaceEscape.Game/Rendering/GameRenderFeature.cs
+++ Games/SpaceEscape/SpaceEscape.Game/Rendering/GameRenderFeature.cs
@@ -47,7 +47,7 @@ namespace SpaceEscape.Rendering
         }
 
         /// <inheritdoc/>
-        public override void PrepareEffectPermutations(RenderThreadContext context)
+        public override void PrepareEffectPermutations(RenderDrawContext context)
         {
             var renderEffects = RootRenderFeature.RenderData.GetData(renderEffectKey);
             int effectSlotCount = ((RootEffectRenderFeature)RootRenderFeature).EffectPermutationSlotCount;
@@ -75,7 +75,7 @@ namespace SpaceEscape.Rendering
         }
 
         /// <inheritdoc/>
-        public unsafe override void Prepare(RenderThreadContext context)
+        public unsafe override void Prepare(RenderDrawContext context)
         {
             foreach (var renderNode in ((RootEffectRenderFeature)RootRenderFeature).RenderNodes)
             {
